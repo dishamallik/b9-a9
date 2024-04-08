1465ea3b330e8319d26a9e14deb9d58149e90fee
dishamallik <dishamallik758@gmail.com>
2024-04-08 14:26:18 +0600

    creating simple website

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXFEHc5h6JLie4M3c6kfAoI-WpPv_m7YI",
+  authDomain: "b9a9-real-estate-caded.firebaseapp.com",
+  projectId: "b9a9-real-estate-caded",
+  storageBucket: "b9a9-real-estate-caded.appspot.com",
+  messagingSenderId: "24512710850",
+  appId: "1:24512710850:web:fc48ec1a60e052eabf935b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
